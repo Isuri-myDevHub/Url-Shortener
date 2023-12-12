ea3853fb2792f494478633eb2e97c6caa6a1110f
isuri <isuriwgunaratne@gmail.com>
2023-12-12 23:54:07 +0530

    Implement Url entity class

--- src/main/java/lk/ijse/dep11/URL_Shortner/modal/Url.java
+++ src/main/java/lk/ijse/dep11/URL_Shortner/modal/Url.java
@@ -1,5 +1,25 @@
 package lk.ijse.dep11.URL_Shortner.modal;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+// Represents a URL entity with fields for the database ID, short URL, and original URL
 public class Url {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String shortUrl;
+    private  String originalUrl;
 
 }
